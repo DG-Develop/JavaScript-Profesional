a88c938002b2a24ec9da3c7a949b0bd674db0ffe
DGDBAND <dgdband@hotmail.com>
2020-07-03 19:03:05 -0500

    promises

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,18 @@
 import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/Autoplay.js'
 
   // query selector agarra un tag o varios en este caso solo uno para video y otro para el boton
-  const video = document.querySelector('video')
-  const player = new MediaPlayer({ el: video })
+const video = document.querySelector('video')
+const player = new MediaPlayer({ el: video, plugins: [
+  /*new AutoPlay()*/
+] })
 
-  const button = document.querySelector('button')
-  button.onclick = () => player.togglePlay()
+const unmute = document.getElementById('unmute')
+unmute.onclick = () =>{
+  (player.media.muted)
+    ? player.unmute()
+    : player.mute()
+} 
+
+const play = document.getElementById('play')
+play.onclick = () => player.togglePlay()
